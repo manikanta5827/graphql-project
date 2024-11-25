44c71a42f75754370ed58f110c9cb3d104b64e23
Abbay <postbox5827@gmail.com>
2024-11-25 15:20:29 +0530

    uri

--- frontend/src/ApolloClient.js
+++ frontend/src/ApolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 console.log(process.env.REACT_APP_GRAPHQL_ENDPOINT);
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  uri: 'https://graphql-project-navy.vercel.app/graphql',
   cache: new InMemoryCache(),
 });
 
